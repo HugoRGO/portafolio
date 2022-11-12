07945d190f423672030f89c4a6df1b8a9f90d50f
HugoRGO <hugarcia.dwfsc7@ucamp.utel.edu.mx>
2022-11-11 20:59:52 -0600

    Commit-3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Figure, Nav, Navbar, Offcanvas } from 'react-bootstrap';
 import './App.css';
+import Profile from './assets/Profile.jpg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {[false].map((expand) => (
+        <Navbar key={expand} bg="secondary" variant="dark" expand={expand} className="mb-3">
+          <Container fluid>
+            <Navbar.Brand href="#" className='fw-bold fst-italic'>Hugo R. García Olmos</Navbar.Brand>
+            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+            <Navbar.Offcanvas
+              id={`offcanvasNavbar-expand-${expand}`}
+              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+              placement="end"
+            >
+              <Offcanvas.Header closeButton className='text-bg-success'>
+                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                </Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body className='px-0'>
+                <Nav className="justify-content-evenly flex-grow-1">
+                  <Figure className='text-center'>
+                    <Figure.Image
+                      className='rounded-circle my-4'
+                      width={180}
+                      height={180}
+                      alt="171x180"
+                      src={Profile}
+                    />
+                    <Figure.Caption className='my-1'>
+                      Nulla vitae elit libero, a pharetra augue mollis interdum.
+                    </Figure.Caption>
+                  </Figure>
+                  <h1 className="justify-content-evenly d-flex text-bg-success py-2 m-0 border-top border-light">Hugo García</h1>
+                  <Nav.Link href="#action1" className="justify-content-evenly d-flex text-light text-bg-success py-2 border-top border-light">Home</Nav.Link>
+                  <Nav.Link href="#action2" className="justify-content-evenly d-flex text-light text-bg-success py-2 border-top border-light">Link</Nav.Link>
+                  <Nav.Link href="#action2" className="justify-content-evenly d-flex text-light text-bg-success py-2 border-top border-light">Link</Nav.Link>
+                </Nav>
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
+          </Container>
+        </Navbar>
+      ))}
+    </>
   );
 }
 
